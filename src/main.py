ac826f613c63920defbb71ef7ef31d85f743d41a
Marcio Nemec <marcio.santos@encora.com>
2023-11-20 19:01:14 -0300

    #5 Temporary change to test configuration in Local Jenkins

--- src/main.py
+++ src/main.py
@@ -5,7 +5,8 @@ import os.path
 def main() -> None:
     logging_config.fileConfig(os.path.join(os.path.dirname(__file__), "logging.ini"), 
                               disable_existing_loggers=False)   
-    data_sync.data_sync()
+    # data_sync.data_sync()
+    logger.info('Test Run ok!')
 
 if __name__ == '__main__':
     main()
