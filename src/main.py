ca2cc6e7b9fce4bbad5285c1f60617c3c176f295
Marcio Nemec <marcio.santos@encora.com>
2023-11-22 13:01:44 -0300

    #5 Update main execution to test in Jenkins

--- src/main.py
+++ src/main.py
@@ -2,13 +2,11 @@ import module.data_synchronization as data_sync
 from logging import config as logging_config
 import os.path
 
-logger = logging.getLogger(__name__)
-
 def main() -> None:
     logging_config.fileConfig(os.path.join(os.path.dirname(__file__), "logging.ini"), 
                               disable_existing_loggers=False)   
     # data_sync.data_sync()
-    logger.info('Test Run ok!')
-
+    print("OK")
+    
 if __name__ == '__main__':
     main()
