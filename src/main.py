8c241132b3950fae059062966245b3cda6018c30
Marcio Nemec <marcio.santos@encora.com>
2023-11-30 09:00:46 -0300

    #5 Organizing import for OS.ENVIRON

--- src/main.py
+++ src/main.py
@@ -1,16 +1,16 @@
 import module.data_synchronization as data_sync
 from logging import config as logging_config
 import os.path
+import os.environ
 
 def main() -> None:
     logging_config.fileConfig(os.path.join(os.path.dirname(__file__), "logging.ini"), 
                               disable_existing_loggers=False)   
     # data_sync.data_sync()
     print("OK")
-    # Testing Secrets variable from Jenkins Credentials Plugin
-    import os
-    print(os.environ['Usr_teste'] + " " + os.environ['Psw_Test'] )
     
+    # Testing Secrets variable from Jenkins Credentials Plugin    
+    print(os.environ['Usr_teste'] + " " + os.environ['Psw_Test'] )    
     
 if __name__ == '__main__':
     main()
