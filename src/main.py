9b499c923a02c5dfdc92ddeb44455d814ea196cb
Marcio Nemec <marcio.santos@encora.com>
2023-12-06 10:59:36 -0300

    #5 Update main.py to include a unit test to Jenkins. Adding JenkinsFile to be used in Pipelines

--- src/main.py
+++ src/main.py
@@ -2,11 +2,8 @@ import module.data_synchronization as data_sync
 from logging import config as logging_config
 import os
 
-def main() -> None:
-    logging_config.fileConfig(os.path.join(os.path.dirname(__file__), "logging.ini"), 
-                              disable_existing_loggers=False)   
-    # data_sync.data_sync()
-    print("OK")
+def do_tests_in_jenkins():
+    print("Starting tests")
     
     # Testing Secrets variable from Jenkins Credentials Plugin    
     print(os.environ['Usr_teste'] + " " + os.environ['Psw_Test'] ) 
@@ -20,6 +17,11 @@ def main() -> None:
         print("Password credential accepted")
     else:
         print("Password credential incorrect")
+
+def main() -> None:
+    logging_config.fileConfig(os.path.join(os.path.dirname(__file__), "logging.ini"), 
+                              disable_existing_loggers=False)   
+    data_sync.data_sync()    
     
 if __name__ == '__main__':
     main()
