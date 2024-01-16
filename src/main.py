bc5b47485d5808951a0bc626fc0791bc9793a24b
Marcio Nemec <marcio.santos@encora.com>
2024-01-16 12:10:43 -0300

    #17: Vault connection through Jenkins Credentials

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import module.data_synchronization as data_sync
 from logging import config as logging_config
-import os
+import os, requests, json
 
 def do_tests_in_jenkins():
-    print("Starting tests")
+    print("Starting Jenkins Tests")
     
     # Testing Secrets variable from Jenkins Credentials Plugin    
     print(os.environ['Usr_teste'] + " " + os.environ['Psw_Test'] ) 
@@ -17,11 +17,24 @@ def do_tests_in_jenkins():
         print("Password credential accepted")
     else:
         print("Password credential incorrect")
+        
+def do_tests_in_vault():
+    print("Starting Vault Tests") 
+    vault_token = os.environ['VltToken'] #From Jenkins Credentials
+    vault_url = os.environ['VltEndPoint'] #From Jenkins Credentials 
+    headers = {'X-Vault-Token': vault_token}
+    res = requests.get(vault_url, headers=headers)
+    print(res.text)
+    j = json.loads(res.text)
+    print(j)
+
+
 
 def main() -> None:
     logging_config.fileConfig(os.path.join(os.path.dirname(__file__), "logging.ini"), 
                               disable_existing_loggers=False)   
-    data_sync.data_sync()    
+    #data_sync.data_sync() 
+    do_tests_in_vault()
     
 if __name__ == '__main__':
     main()
