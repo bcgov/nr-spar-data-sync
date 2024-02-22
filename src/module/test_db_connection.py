3106b370159921673ce3fd9d361db990755e98f1
Marcio Nemec <marcio.santos@encora.com>
2024-02-22 15:23:15 -0300

    #17 fixing URL connection parameter

--- src/module/test_db_connection.py
+++ src/module/test_db_connection.py
@@ -1,51 +1,31 @@
 import oracledb
 from sqlalchemy import create_engine, text
+from sqlalchemy.engine import URL
 
 class test_db_connection:
     """ Class used to test database connections. """
     def do_test(database_config):
         """ Test Connection """
-        #connection_string = test_db_connection.format_connection_string(database_config)
-        engine = test_db_connection.create_engine_by_type(database_config)
+        connection_string = test_db_connection.format_connection_string(database_config)
+        print(connection_string)
+        engine = create_engine(connection_string)
         conn = engine.connect().execution_options(autocommit=False)
         result = conn.execute(database_config["test_query"])
-        return result
+        return result        
         
-    def create_engine_by_type(database_config: str):
-        if database_config['type'] == 'ORACLE':
-           return create_engine(
-               f'oracle+oracledb://:@',
-               thick_mode=False,
-               connect_args={
-                   "user": username,
-                   "password": password,
-                   "host": cp.host,
-                   "port": cp.port,
-                   "service_name": cp.service_name
-            })
-
-        if database_config['type'] == 'POSTGRES':
-            connection_string = 'postgresql+psycopg2://{}:{}@{}:{}/{}'.format(
-                database_config['username'], 
-                database_config['password'], 
-                database_config['host'], 
-                database_config['port'],
-                database_config['database'])
-            return create_engine(connection_string)
-
-        
-"""    def format_connection_string(database_config: str):
+    def format_connection_string(database_config: str):
         """ Formats the connection string based on the database type and the connection configuration. """
         if database_config['type'] == 'ORACLE':
-            connection_string = ('oracle+oracledb://:@',
-                thick_mode=False, #ERROR HERE. WHY?
-                connect_args={
-                    "user": database_config['username'],
-                    "password": database_config['password'],
-                    "host": database_config['host'],
-                    "port": database_config['port'],
-                    "service_name": database_config['service_name']
-                }
+            cp = oracledb.ConnectParams(
+                host=database_config['host'],
+                port=database_config['port'],
+                service_name=database_config['service_name'])
+                
+            connection_string = URL.create(
+                "oracle+oracledb",
+                host=cp.get_connect_string(),
+                username=database_config['username'],
+                password=database_config['password'],
             )
 
         if database_config['type'] == 'POSTGRES':
@@ -57,4 +37,3 @@ class test_db_connection:
                 database_config['database'])
 
         return connection_string
-"""
