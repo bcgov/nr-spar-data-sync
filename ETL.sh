30176c32875f869cc11b0b16211680295602acf6
Marcio Nemec <marcio.santos@encora.com>
2024-03-05 08:35:26 -0300

    #17 Run ETL.sh instead image running commands

---
+++ ETL.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./oc_bin/oc version
+./oc_bin/oc login --token=$oct --server=$ocs
+./oc_bin/oc project $ocp
+./oc_bin/oc port-forward $ocpd $ocpf:$ocpt
+
+
+python3 src/main.py $test_mode
