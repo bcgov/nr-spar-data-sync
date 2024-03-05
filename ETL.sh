f001b86f83c6ee190deb9a5cf38bd1b1af27caa6
Marcio Nemec <marcio.santos@encora.com>
2024-03-05 11:13:01 -0300

    #17 Changing OC context

--- ETL.sh
+++ ETL.sh
@@ -3,9 +3,12 @@
 ./oc_bin/oc version
 echo $oct 
 echo $ocs
-./oc_bin/oc login --token=$oct --server=$ocs
+./oc_bin/oc config set-context `oc config current-context` --namespace=$ocp
+./oc_bin/oc login $ocs --token=$oct
 ./oc_bin/oc project $ocp
-./oc_bin/oc port-forward $ocpd $ocpf:$ocpt &
+export postgresPod=$(./oc_bin/oc get pods | grep database | awk '{print $1}')
+echo $postgresPod
+./oc_bin/oc port-forward $postgresPod $ocpf:$ocpt &
 
 
 python3 src/main.py $test_mode
