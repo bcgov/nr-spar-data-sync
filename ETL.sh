1f658551be11a789575999e2d105154cb060f14d
Marcio Nemec <marcio.santos@encora.com>
2024-03-05 08:51:26 -0300

    #17 OC separation

--- ETL.sh
+++ ETL.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 ./oc_bin/oc version
+echo $oct 
+echo $ocs
 ./oc_bin/oc login --token=$oct --server=$ocs
 ./oc_bin/oc project $ocp
-./oc_bin/oc port-forward $ocpd $ocpf:$ocpt
+./oc_bin/oc port-forward $ocpd $ocpf:$ocpt &
 
 
 python3 src/main.py $test_mode
